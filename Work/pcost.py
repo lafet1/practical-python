92fe0d8ee1f9a63ef893de4ab569423be54b616b
stepan_svoboda <stepan.svo@seznam.cz>
2020-06-08 15:39:28 +0200

    1_27 solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+overall_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as file:
+    next(file)
+    for line in file:
+        _, no_shares, price = line.split(',')
+        overall_cost += int(no_shares) * float(price)
+
+print(f'Total cost {overall_cost}')
